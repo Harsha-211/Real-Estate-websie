96837ca174c2a4a62df34f6511785a5831f943a8
Harsha-211 <harsha.21bce8036@vitapstudent.ac.in>
2023-11-21 15:49:00 +0530

    Created Midleware and function to handle possible errors

--- api/conrollers/auth.controller.js
+++ api/conrollers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async(req,res) => {
+export const signup = async(req,res,next) => {
     const {usernmae , email , password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({username , email , password: hashedPassword});
@@ -9,7 +10,7 @@ export const signup = async(req,res) => {
         await newUser.save();
         res.status.json("User created sucsessfully..!");
     }
-    catch(err){
-        res.status.json(error.message);
+    catch(error){
+        next(error);
     }
 };
